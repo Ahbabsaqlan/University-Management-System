9eafd9523cf7c6df62bde02e8514c36607fc250b
Ahbabsaqlan <ahbabzami3@gmail.com>
2024-05-11 00:22:51 +0600

    Login And Dashhboard

---
+++ login.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace University_Management_System
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Minimize_btn_Click(object sender, EventArgs e)
+        {
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+
+        }
+
+        private void Login_btn_Click(object sender, EventArgs e)
+        {
+            DashBoard dashBoard = new DashBoard();
+            dashBoard.Show();
+            this.Hide();
+        }
+    }
+}
